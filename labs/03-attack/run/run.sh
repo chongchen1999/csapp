ff4270a199e3eb243bbd67db2df58482f8172eb2
Chong Chen <chongchen1999@gmail.com>
2024-10-05 19:39:56 -0400

    update files

--- labs/03-attack/run/run.sh
+++ labs/03-attack/run/run.sh
@@ -1,6 +1,6 @@
-# ./../hex2raw < ctarget_l1/ctarget_l1.txt > ctarget_l1/ctarget_l1_raw.txt && ./../ctarget -q -i ctarget_l1/ctarget_l1_raw.txt
-# ./../hex2raw < ctarget_l2/ctarget_l2.txt > ctarget_l2/ctarget_l2_raw.txt && ./../ctarget -q -i ctarget_l2/ctarget_l2_raw.txt
-# ./../hex2raw < ctarget_l3/ctarget_l3.txt > ctarget_l3/ctarget_l3_raw.txt && ./../ctarget -q -i ctarget_l3/ctarget_l3_raw.txt
+./../hex2raw < ctarget_l1/ctarget_l1.txt > ctarget_l1/ctarget_l1_raw.txt && ./../ctarget -q -i ctarget_l1/ctarget_l1_raw.txt
+./../hex2raw < ctarget_l2/ctarget_l2.txt > ctarget_l2/ctarget_l2_raw.txt && ./../ctarget -q -i ctarget_l2/ctarget_l2_raw.txt
+./../hex2raw < ctarget_l3/ctarget_l3.txt > ctarget_l3/ctarget_l3_raw.txt && ./../ctarget -q -i ctarget_l3/ctarget_l3_raw.txt
 
-# ./../hex2raw < rtarget_l2/rtarget_l2.txt > rtarget_l2/rtarget_l2_raw.txt && ./../rtarget -q -i rtarget_l2/rtarget_l2_raw.txt
+./../hex2raw < rtarget_l2/rtarget_l2.txt > rtarget_l2/rtarget_l2_raw.txt && ./../rtarget -q -i rtarget_l2/rtarget_l2_raw.txt
 ./../hex2raw < rtarget_l3/rtarget_l3.txt > rtarget_l3/rtarget_l3_raw.txt && ./../rtarget -q -i rtarget_l3/rtarget_l3_raw.txt
