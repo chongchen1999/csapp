cb3f50a3c27f772f9e9ee804f793e784642b98b6
Chong Chen <chongchen1999@gmail.com>
2024-10-18 14:33:19 -0400

    update

--- codes/03-machine/stack-storage/run/run.sh
+++ codes/03-machine/stack-storage/run/run.sh
@@ -1,5 +1,16 @@
-gcc -o caller ../caller.c -Og
-gcc -o call_proc ../call_proc.c -Og
+#!/bin/bash
 
+# Define optimization level variable (e.g., -Og, -O2, -O3)
+OPTIMIZATION=-Og
+
+# Compile the source files with the chosen optimization level
+gcc -o caller ../caller.c $OPTIMIZATION
+gcc -o call_proc ../call_proc.c $OPTIMIZATION
+
+# Generate assembly code with the chosen optimization level
+gcc -S ../caller.c $OPTIMIZATION
+gcc -S ../call_proc.c $OPTIMIZATION
+
+# Generate disassembly from the object files
 objdump -d caller > caller.d
 objdump -d call_proc > call_proc.d
